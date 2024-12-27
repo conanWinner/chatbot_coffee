d4fff0b6c64c70289c9b9f779587f654159a9371
conanWinner <doanquangthangqt04@gmail.com>
2024-12-28 01:05:46 +0700

    chang port

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 class Config:
     DEBUG = True
     HOST = '0.0.0.0'
-    PORT = 5000
+    PORT = 4000
     MONGO_URI = "mongodb+srv://conanWinner:uWtTfmmG2UEfA5Am@atlascluster.thds0wq.mongodb.net/?retryWrites=true&w=majority"
